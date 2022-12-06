a4d1c110d7806466e2a983358e8af4ecff19c27e
Enej <mastnak.enej@gmail.com>
2022-12-06 13:53:32 +0100

    Day 6 solution AOC2022 in C#

---
+++ DaySix/Program.cs
@@ -0,0 +1,9 @@
+﻿using DaySix;
+
+string lines = File.ReadAllText("input.txt").ReplaceLineEndings("");
+
+int partOneSolution = Solution.PartOneSolution(lines);
+Console.WriteLine(partOneSolution);
+
+int partTwoSolution = Solution.PartTwoSolution(lines);
+Console.WriteLine(partTwoSolution);
---
+++ DaySix/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace DaySix;
+
+public class Solution
+{
+    public static int PartOneSolution(string lines)
+    {
+        
+        int numProcessing = 0;
+
+        while (numProcessing < lines.Length)
+        {
+
+            string subString = lines.Substring(numProcessing, 4);
+            HashSet<char> subStringSet = new HashSet<char>(subString);
+            if (subStringSet.Count == 4)
+                break;
+            numProcessing++;
+
+        }
+        // Add +1 for the last proccesed element, +3 for the last character that represents a marker
+        return numProcessing + 4;
+    }
+    public static int PartTwoSolution(string lines)
+    {
+        
+        int numProcessing = 0;
+
+        while (numProcessing < lines.Length)
+        {
+
+            string subString = lines.Substring(numProcessing, 14);
+            HashSet<char> subStringSet = new HashSet<char>(subString);
+            if (subStringSet.Count == 14)
+                break;
+            numProcessing++;
+
+        }
+        // Add +1 for the last proccesed element, +13 for the last character that represents a marker
+        return numProcessing + 14;
+    }
+}
